7f881568f1614cf50168fd7100de17d6b231ce03
heathermcn <hgmcn@live.com>
2025-03-19 22:22:30 -0500

    preliminary footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar from './components/NavBar/NavBar.jsx'
+import Footer from './components/Footer/Footer.jsx'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Footer/>
     </div>
   )
 };
