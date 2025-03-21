21289976fb71dcfe5ab780969960971176f0d843
heathermcn <hgmcn@live.com>
2025-03-21 09:08:48 -0500

    replace footer links with icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Footer.css';
+import instagram from '../../assets/instagram.png'
+import youtube from '../../assets/youtube.png'
+import tumblr from '../../assets/tumblr.png'
+import twitter from '../../assets/twitter.png'
 
 const Footer = () => {
   return (
@@ -8,19 +12,27 @@ const Footer = () => {
       <div className="footer-center">
         <ul className="nav-links">
           <li>
-            <a href="https://www.instagram.com/rogueheartcosplay/" target="_blank">Instagram</a>
+            <a href="https://www.instagram.com/rogueheartcosplay/" target="_blank">
+              <img src={instagram} alt="Instagram"/>
+            </a>
           </li>
           <li>
-            <a href="https://www.youtube.com/@rogueheartcosplay" target="_blank">Youtube</a>
+            <a href="https://www.youtube.com/@rogueheartcosplay" target="_blank">
+              <img src={youtube} alt="Youtube"/>
+            </a>
           </li>
           <li>
-            <a href="https://rogueheartcosplay.tumblr.com" target="_blank">Tumblr</a>
+            <a href="https://rogueheartcosplay.tumblr.com" target="_blank">
+              <img src={tumblr} alt="Tumblr"/>
+            </a>
           </li>
           <li>
-            <a href="https://x.com/rogueheartcos" target="_blank">Twitter</a>
+          <a href="https://x.com/rogueheartcos" target="_blank">
+            <img src={twitter} alt="Twitter"/>
+          </a>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            {/* <a href="/contact">Contact</a> */}
           </li>
         </ul>
       </div>
