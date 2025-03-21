156950ddd75b7522663ba31b9dbe38230b7dc2d3
heathermcn <hgmcn@live.com>
2025-03-20 21:50:54 -0500

    update navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,28 +2,31 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    return (
-        <nav className="footer">
-            <div className="footer-left">
-                <a href="/" className="logo">
-                    Rogue Heart Cosplay
-                </a>
-            </div>
-            <div className="footer-right">
-                <ul className="nav-links">
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="/projects">Projects</a>
-                    </li>
-                    <li>
-                        <a href="/contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
-  };
+  return (
+    <nav className="footer">
+      <div className="footer-left"/>
+      <div className="footer-center">
+        <ul className="nav-links">
+          <li>
+            <a href="https://www.instagram.com/rogueheartcosplay/" target="_blank">Instagram</a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/@rogueheartcosplay" target="_blank">Youtube</a>
+          </li>
+          <li>
+            <a href="https://rogueheartcosplay.tumblr.com" target="_blank">Tumblr</a>
+          </li>
+          <li>
+            <a href="https://x.com/rogueheartcos" target="_blank">Twitter</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-right"/>
+    </nav>
+  );
+};
 
 export default Footer;
