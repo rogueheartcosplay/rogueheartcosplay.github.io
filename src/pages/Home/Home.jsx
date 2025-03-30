c0fc7c5a50be4aa689a6fb0f6e137151b5d2efa7
heathermcn <hgmcn@live.com>
2025-03-30 15:24:52 -0500

    adjust alignment for homepage text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,8 +7,10 @@ function Home() {
   return (
     <div className="home">
       <Hero/>
-      <h1>Welcome!</h1>
-      <h2>This site is a work in progress (just like my costumes...)</h2>
+      <div className="content">
+        <h1>Welcome!</h1>
+        <h2>This site is a work in progress (just like my costumes...)</h2>
+      </div>
     </div>
   )
 };
