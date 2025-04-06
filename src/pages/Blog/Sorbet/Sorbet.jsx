7e2f19dee56b64cd455277719b0bf85318ef7ddb
heathermcn <hgmcn@live.com>
2025-04-05 20:26:51 -0500

    add images and sorbet tutorial

--- src/pages/Blog/Sorbet/Sorbet.jsx
+++ src/pages/Blog/Sorbet/Sorbet.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
 import Hero from '../../../components/Hero/Hero';
 import Media from '../../../components/Media/Media';
-import DSCF0405 from '../../../assets/DSCF0405.jpg';
+import Exorcism_Sorbet from '../../../assets/Exorcism_Sorbet.jpg'
+import exorcismSorbet from '../../../assets/exorcismSorbet.jpg'
+import Treasure_insects_color from '../../../assets/Treasure_insects_color.png'
+import dm6 from '../../../assets/dm6.jpg'
+import IMG_7787 from "../../../assets/IMG_7787.jpeg"
+import IMG_7788 from "../../../assets/IMG_7788.jpeg"
+import IMG_7789 from "../../../assets/IMG_7789.jpeg"
+import IMG_7790 from "../../../assets/IMG_7790.jpeg"
+import IMG_7791 from "../../../assets/IMG_7791.jpeg"
+import IMG_7793 from "../../../assets/IMG_7793.jpeg"
+import IMG_7794 from "../../../assets/IMG_7794.jpeg"
+import IMG_7795 from "../../../assets/IMG_7795.jpeg"
+import IMG_7796 from "../../../assets/IMG_7796.jpeg"
+import IMG_7797 from "../../../assets/IMG_7797.jpeg"
+import IMG_7798 from "../../../assets/IMG_7798.jpeg"
+import IMG_7799 from "../../../assets/IMG_7799.jpeg"
+import IMG_7800 from "../../../assets/IMG_7800.jpeg"
+import IMG_7801 from "../../../assets/IMG_7801.jpeg"
+import IMG_7802 from "../../../assets/IMG_7802.jpeg"
+import IMG_7803 from "../../../assets/IMG_7803.jpeg"
+import IMG_7804 from "../../../assets/IMG_7804.jpeg"
+import IMG_7805 from "../../../assets/IMG_7805.jpeg"
+import IMG_7807 from "../../../assets/IMG_7807.jpeg"
+import IMG_7808 from "../../../assets/IMG_7808.jpeg"
+import IMG_7809 from "../../../assets/IMG_7809.jpeg"
+import IMG_7810 from "../../../assets/IMG_7810.jpeg"
+import IMG_7811 from "../../../assets/IMG_7811.jpeg"
+import IMG_7812 from "../../../assets/IMG_7812.jpeg"
+import IMG_7813 from "../../../assets/IMG_7813.jpeg"
+import IMG_7814 from "../../../assets/IMG_7814.jpeg"
+import IMG_7815 from "../../../assets/IMG_7815.jpeg"
+import IMG_7816 from "../../../assets/IMG_7816.jpeg"
+import IMG_7817 from "../../../assets/IMG_7817.jpeg"
+import IMG_7818 from "../../../assets/IMG_7818.jpeg"
+import IMG_7819 from "../../../assets/IMG_7819.jpeg"
+import IMG_7833 from "../../../assets/IMG_7833.jpeg"
+import IMG_7834 from "../../../assets/IMG_7834.jpeg"
+import IMG_7835 from "../../../assets/IMG_7835.jpeg"
+import IMG_7836 from "../../../assets/IMG_7836.jpeg"
+import IMG_7839 from "../../../assets/IMG_7839.jpeg"
+import IMG_7840 from "../../../assets/IMG_7840.jpeg"
+import IMG_7841 from "../../../assets/IMG_7841.jpeg"
+import IMG_7847 from "../../../assets/IMG_7847.jpeg"
+import IMG_7858 from "../../../assets/IMG_7858.jpeg"
+import IMG_7859 from "../../../assets/IMG_7859.jpeg"
+import IMG_7860 from "../../../assets/IMG_7860.jpeg"
+import IMG_7861 from "../../../assets/IMG_7861.jpeg"
+import IMG_7862 from "../../../assets/IMG_7862.jpeg"
+import IMG_7863 from "../../../assets/IMG_7863.jpeg"
+import IMG_7864 from "../../../assets/IMG_7864.jpeg"
+import IMG_7871 from "../../../assets/IMG_7871.jpeg"
+import IMG_7873 from "../../../assets/IMG_7873.jpeg"
+
 import './Sorbet.css';
 
 function Sorbet() {
@@ -14,13 +66,18 @@ function Sorbet() {
             <h2>Cosplay Prop Tutorial</h2>
         </div>
         <Media>
-          <img src={DSCF0405} alt="Rogue Heart Cosplay standing in the forest wearing the Wavemother's Robe." />
+          <img src={Exorcism_Sorbet} alt="A bowl of colorful sorbet from Delicious In Dugeon"/>
+          <img src={exorcismSorbet} alt="A real life prop version of colorful sorbet from Delicious In Dugeon"/>
+          <img src={IMG_7787} alt="Craft supplies"/>
         </Media>
         <div className="content">
           <div>
+            <h3>The Project</h3>
+              <p>I was inspired by the anime & manga Delicious In Dungeon (Dungeon Meshi) by Ryoko Kui to make my own "Exorcism Sorbet" — a tasty dessert to ward off ghosts.</p>
+              <p>My version is a non-edible prop to go along with my Izutsumi cosplay. It's a great weekend project!</p>
             <h3>Tools & Materials</h3>
-              <p>I used a combination of materials from my local craft store, the thrift store, and items I already owned. Here's the full "start from scratch" list.</p>
-              <p>Tools</p>
+              <p>I used a combination of materials from my local craft store, the thrift store, and items I already owned. Here's the full "starting from scratch" list.</p>
+              <p>Tools - $24</p>
               <ol>
                 <li>
                 Hot glue gun - $15
@@ -32,7 +89,7 @@ function Sorbet() {
                 Paint brush - $2
                 </li>
               </ol>
-              <p>Materials</p>
+              <p>Materials - $32</p>
               <ol>
                 <li>
                 Watercolor paint - $5
@@ -65,29 +122,142 @@ function Sorbet() {
               <p>Total Cost - $56</p>
               </div>
               <div>
-            <h3>Instructions</h3>
-            <p>1. Cut styrofoam spheres in half</p>
-            <p>2. Glue half-spheres into the bowl with hot glue</p>
-            <p>3. Use leftover scraps of foam to fill in large gaps</p>
-            <p>4. Carve the next layer of spheres to fit snugly above the first layer</p>
-            <p>5. Glue the next layer in place</p>
-            <p>6. Once again, fill in any large gaps with scrap foam</p>
-            <p>7. Add some “melted sorbet" to the edges and small gaps with hot glue and confetti</p>
-            <p>8. After waiting for the hot glue to dry, pour off any excess confetti</p>
-            <p>9. Prepare your “treasure bugs” (coins and gems) to add to the sorbet</p>
-            <p>10. Glue the coins and gems to the foam spheres</p>
-            <p>11. Score lines into the foam spheres to mimic the look of real sorbet</p>
-            <p>12. Use watercolor paints to color the foam spheres</p>
-            <p>13. Let the spheres dry for at least a few hours</p>
-            <p>14. Cover a small section of foam in a thick layer of mod podge</p>
-            <p>15. Cover the mod podge in confetti, similar to the hot glue method</p>
-            <p>16. After waiting a couple minutes, pour off any excess confetti</p>
-            <p>17. Repeat steps 14-16 until all foam surfaces are covered in confetti</p>
-            <p>18. Let the sorbet dry over night</p>
-            <p>19. Voila! Enjoy a nutritious bowl of Exorcism Sorbet</p>
-            <p>20. Optional: Repeat this process with a spoon instead of a bowl to look like a scoop of sorbet</p>
+              </div>
             </div>
-        </div>
+            <div className="content">
+            <h3>Instructions</h3>
+            <p><strong>1.</strong> Cut styrofoam spheres in half</p>
+            </div>
+            <Media>
+              <img src={IMG_7788} alt="Craft supplies"/>
+              <img src={IMG_7789} alt="Craft supplies"/>
+              <img src={IMG_7791} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>2.</strong>  Glue half-spheres into the bowl with hot glue</p>
+            </div>
+            <Media>
+              <img src={IMG_7790} alt="Craft supplies"/>
+              <img src={IMG_7793} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>3.</strong>  Use leftover scraps of foam to fill in large gaps</p>
+            </div>
+            <Media>
+              <img style={{width:'31%'}} src={IMG_7795} alt="Craft supplies"/>
+              <img style={{width:'31%'}} src={IMG_7796} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>4.</strong>  Carve the next layer of spheres to fit snugly above the first layer</p>
+            </div>
+            <Media>
+              <img src={IMG_7797} alt="Craft supplies"/>
+              <img src={IMG_7798} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>5.</strong>  Glue the next layer in place</p>
+            <p>Once again, fill in any large gaps with scrap foam</p>
+            </div>
+            <Media>
+              <img style={{width:'31%'}} src={IMG_7799} alt="Craft supplies"/>
+              <img style={{width:'31%'}} src={IMG_7800} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>6.</strong>  Add some “melted sorbet" to the edges and small gaps with hot glue and confetti</p>
+            </div>
+            <Media>
+              <img src={IMG_7801} alt="Craft supplies"/>
+              <img src={IMG_7802} alt="Craft supplies"/>
+              <img src={IMG_7803} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>7.</strong>  After waiting a few minutes for the hot glue to cool, pour off any excess confetti</p>
+            </div>
+            <Media>
+              <img style={{width:'31%'}} src={IMG_7805} alt="Craft supplies"/>
+              <img style={{width:'31%'}} src={IMG_7807} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>8.</strong>  Prepare your “treasure insects" (coins and gems) to add to the sorbet</p>
+            </div>
+            <Media>
+              <img src={IMG_7816} alt="Craft supplies"/>
+              <img src={IMG_7818} alt="Craft supplies"/>
+              <img src={IMG_7809} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>9.</strong>  Glue the coins and gems to the foam spheres</p>
+            </div>
+            <Media>
+              <img src={IMG_7810} alt="Craft supplies"/>
+              <img src={IMG_7811} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>10.</strong>  Repeat steps 6-7 on the edges of the coins and gems</p>
+            </div>
+            <Media>
+              <img src={IMG_7812} alt="Craft supplies"/>
+              <img src={IMG_7814} alt="Craft supplies"/>
+              <img src={IMG_7815} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>11.</strong>  Score lines into the foam spheres to mimic the look of real sorbet</p>
+            </div>
+            <Media>
+              <img src={IMG_7833} alt="Craft supplies"/>
+              <img src={IMG_7834} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>12.</strong>  Use watercolor paints to color the foam spheres</p>
+            </div>
+            <Media>
+              <img style={{width:'31%'}} src={IMG_7835} alt="Craft supplies"/>
+              <img style={{width:'31%'}} src={IMG_7836} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>13.</strong>  Use a paper towel to dab off extra paint</p>
+            </div>
+            <Media>
+              <img src={IMG_7839} alt="Craft supplies"/>
+              <img src={IMG_7840} alt="Craft supplies"/>
+              <img src={IMG_7841} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>14.</strong>  Let the spheres dry for at least a few hours</p>
+            </div>
+            <Media>
+              <img src={IMG_7847} alt="Craft supplies"/>
+              <img src={IMG_7858} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>15.</strong>  Cover a small section of foam in a thick layer of mod podge</p>
+            </div>
+            <Media>
+              <img src={IMG_7859} alt="Craft supplies"/>
+              <img src={IMG_7860} alt="Craft supplies"/>
+              <img src={IMG_7861} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>16.</strong>  Cover the mod podge in confetti, similar to the hot glue method</p>
+            </div>
+            <Media>
+              <img src={IMG_7862} alt="Craft supplies"/>
+              <img src={IMG_7863} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>17.</strong>  Repeat steps 15-16 until all foam surfaces are covered in confetti</p>
+            </div>
+            <Media>
+              <img style={{width:'31%'}} src={IMG_7871} alt="Craft supplies"/>
+              <img style={{width:'31%'}} src={IMG_7873} alt="Craft supplies"/>
+            </Media>
+            <div className="content">
+            <p><strong>18.</strong> Let it dry over night... and voila! Enjoy a bowl of Exorcism Sorbet</p>
+            </div>
+            <Media>
+              <img style={{width:'31%'}} src={exorcismSorbet} alt="A real life prop version of colorful sorbet from Delicious In Dugeon"/>
+              <img style={{width:'31%'}} src={dm6} alt="Rogue Heart Cosplay dressed as Izutsumi holding a bowl of sorbet."/>
+            </Media>
     </div>
   )
 };
